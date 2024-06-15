7569979dbba0c3690104dbb8482eee5dd2f94236
Eric Hartley <spiffydude0@gmail.com>
2024-06-15 16:17:32 -0500

    complete code

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?"); // output
+            var name = Console.ReadLine(); // input
+
+            Console.Write($"Hello, {name}. "); // output
+
+
+            Console.WriteLine("What is your favorite color?"); // output
+            var FavColor = Console.ReadLine(); // input
+
+            Console.WriteLine("What is your favorite animal?"); // output
+            var FavAnimal = Console.ReadLine(); // input
+
+            Console.WriteLine("What is your favorite band?"); // output
+            var FavBand = Console.ReadLine(); // input
+
         }
     }
 }
